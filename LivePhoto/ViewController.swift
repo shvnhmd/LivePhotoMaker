d7ba1bf1b97f5fe33f1c7fc224b1e49e4dd2de15
Ikhtiar Ahmed <shvnhmd@gmail.com>
2020-12-28 15:27:34 +0600

    Initial Commit

---
+++ LivePhoto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LivePhoto
+//
+//  Created by Ikhtiar Ahmed on 12/28/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
